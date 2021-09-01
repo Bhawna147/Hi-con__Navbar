69df8da733dbb8357b1c43a2a96a4e2d62506429
Bhawna147 <Khatribhawna77@gmail.com>
2021-09-01 14:04:37 +0530

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "./Navbar.css";
+import { IconContext } from "react-icons/lib";
+import { FaSearch } from "react-icons/fa";
+import { BsFillChatDotsFill , BsPeopleCircle , BsBellFill} from "react-icons/bs";
+import logo from './logo.png'
+
+
+export const Navbar = () => {
+    return (
+      
+          <>
+
+          <nav className = "main-nav">
+              {/* 1st logo  */}
+
+            <div className="logoo">
+
+            <img src={logo} alt="logo" />    
+
+            </div>
+            
+
+
+            {/* 2nd Search */}
+
+            <div className="mid">
+            {/* <h1>mid</h1> */}
+
+            </div>
+            
+
+
+            {/* 3rd Icons */}
+            
+            <IconContext.Provider value = { {color: "Black"   ,  size: "2em" }}>
+            <div className="right">
+               
+               <ul className="right-desktop">
+                 
+                 <input type="text" placeholder="Search" className="search"/> 
+
+                <li><a href="" target = "_blank" >                           {/* notification*/}
+                <BsBellFill/>  
+                </a></li> 
+
+                <li><a href="" target="_blank" >                             {/* messages/chat */}
+                <BsFillChatDotsFill/>  
+                </a></li>  
+
+                <li><a href="" target="_blank" >                             {/* user profile */}
+                <BsPeopleCircle/>  
+                </a></li> 
+
+               </ul>
+
+           </div>
+           </ IconContext.Provider>
+
+           </nav>
+
+        
+
+         </>
+        
+    );
+};
+
+
+export default Navbar;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import reactDOM from "react-dom";
+import Navbar from './components/Navbar'
+
+reactDOM.render(
+    <div>
+      <Navbar />
+    </div>,
+    document.getElementById("root")
+  );
+
